959e6125041eb89893ffad07b82866479734e5f0
badhei <Swaraj.Badhei-EE@infineon.com>
2022-01-31 09:47:02 +0530

    bare minimum code to regenrate issue.

---
+++ python_dir/python_module_dir/python_functions.py
@@ -0,0 +1,11 @@
+import os
+
+def function_1():
+    """[This function just prints out a sentence.]
+    """
+    print("entered into function_1.")
+
+def function_2():
+    """[This function prints something else.]
+    """
+    print("Reached inside second function")
